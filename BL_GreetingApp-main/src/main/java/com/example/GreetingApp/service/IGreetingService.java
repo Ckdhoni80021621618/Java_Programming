ca3d3b3945f8574cc5fb1423763b892b094659ef
Amrendra singh <94617769+Ckdhoni80021621618@users.noreply.github.com>
2025-03-30 16:08:40 +0530

    Add files via upload

---
+++ BL_GreetingApp-main/src/main/java/com/example/GreetingApp/service/IGreetingService.java
@@ -0,0 +1,9 @@
+package com.example.GreetingApp.service;
+
+import com.example.GreetingApp.model.Greeting;
+import com.example.GreetingApp.model.AuthUser;
+
+public interface IGreetingService {
+    Greeting addGreeting(AuthUser user);
+    Greeting getGreetingById(long id);
+}
